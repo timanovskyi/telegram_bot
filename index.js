e0f7ada48810acc829a886344b9153d895c53eff
egor <megalomania281@gmail.com>
2022-12-12 21:00:19 +0100

    renamed file and variable

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const TelegramBot = require("node-telegram-bot-api");
 
 const { RU, UA, EN, PL } = require("./translations");
 const BIBLE = require("./bibleTexts.json");
-const PERMISSIONS = require("./projects_permissions");
+const CONF = require("./conf");
 
-const bot = new TelegramBot(PERMISSIONS.token, { polling: true });
+const bot = new TelegramBot(CONF.token, { polling: true });
 const BIBLE_PARSE = JSON.parse(JSON.stringify(BIBLE));
 
 let CURRENT_LANGUAGE = RU;
@@ -66,26 +66,26 @@ const checkMainMenu = async (chatId, text) => {
   switch (text) {
     case CURRENT_LANGUAGE.mainMenu.donation: {
       await bot.sendMessage(chatId, CURRENT_LANGUAGE.donation.recipient);
-      await bot.sendMessage(chatId, PERMISSIONS.donation.recipient);
+      await bot.sendMessage(chatId, CONF.donation.recipient);
       await bot.sendMessage(chatId, CURRENT_LANGUAGE.donation.number);
-      await bot.sendMessage(chatId, PERMISSIONS.donation.number);
+      await bot.sendMessage(chatId, CONF.donation.number);
       await bot.sendMessage(chatId, CURRENT_LANGUAGE.donation.title);
-      return bot.sendMessage(chatId, PERMISSIONS.donation.title);
+      return bot.sendMessage(chatId, CONF.donation.title);
     }
     case CURRENT_LANGUAGE.mainMenu.feedback: {
       return bot.sendMessage(
         chatId,
-        `${CURRENT_LANGUAGE.ourFeedback}\n${PERMISSIONS.urls.feedback}`,
+        `${CURRENT_LANGUAGE.ourFeedback}\n${CONF.urls.feedback}`,
         { disable_web_page_preview: true }
       );
     }
     case CURRENT_LANGUAGE.mainMenu.churchInfo: {
-      await bot.sendPhoto(chatId, PERMISSIONS.urls.churchInfo, {
+      await bot.sendPhoto(chatId, CONF.urls.churchInfo, {
         caption: `${CURRENT_LANGUAGE.churchInfo}`,
       });
       return bot.sendMessage(
         chatId,
-        `${CURRENT_LANGUAGE.pastorsInfo.moreInfo}\n${PERMISSIONS.urls.church}`,
+        `${CURRENT_LANGUAGE.pastorsInfo.moreInfo}\n${CONF.urls.church}`,
         { disable_web_page_preview: true }
       );
     }
@@ -94,40 +94,40 @@ const checkMainMenu = async (chatId, text) => {
         chatId,
         `
       ${CURRENT_LANGUAGE.followUs}
-      \n${CURRENT_LANGUAGE.viber}: ${PERMISSIONS.urls.viberOffer}
-      \n${CURRENT_LANGUAGE.telegram}: ${PERMISSIONS.urls.telegaOffer}
-      \n${CURRENT_LANGUAGE.instagram}: ${PERMISSIONS.urls.instagramOffer}`,
+      \n${CURRENT_LANGUAGE.viber}: ${CONF.urls.viberOffer}
+      \n${CURRENT_LANGUAGE.telegram}: ${CONF.urls.telegaOffer}
+      \n${CURRENT_LANGUAGE.instagram}: ${CONF.urls.instagramOffer}`,
         {
           disable_web_page_preview: true,
         }
       );
     }
     case CURRENT_LANGUAGE.mainMenu.events: {
-      await bot.sendPhoto(chatId, PERMISSIONS.urls.homeGroupPhoto, {
-        caption: `${CURRENT_LANGUAGE.homeGroup}${PERMISSIONS.homeGroup.address}`,
+      await bot.sendPhoto(chatId, CONF.urls.homeGroupPhoto, {
+        caption: `${CURRENT_LANGUAGE.homeGroup}${CONF.homeGroup.address}`,
       });
       return bot.sendMessage(
         chatId,
-        `${CURRENT_LANGUAGE.moreEvents}\n${PERMISSIONS.urls.insta}`
+        `${CURRENT_LANGUAGE.moreEvents}\n${CONF.urls.insta}`
       );
     }
     case CURRENT_LANGUAGE.mainMenu.churchLeaders: {
-      await bot.sendPhoto(chatId, PERMISSIONS.info.pastor_1.photo, {
-        caption: `${CURRENT_LANGUAGE.pastorsInfo.title}\n${PERMISSIONS.info.pastor_1.name}\n${CURRENT_LANGUAGE.pastorsInfo.tel}${PERMISSIONS.info.pastor_1.tel}`,
+      await bot.sendPhoto(chatId, CONF.info.pastor_1.photo, {
+        caption: `${CURRENT_LANGUAGE.pastorsInfo.title}\n${CONF.info.pastor_1.name}\n${CURRENT_LANGUAGE.pastorsInfo.tel}${CONF.info.pastor_1.tel}`,
       });
-      await bot.sendPhoto(chatId, PERMISSIONS.info.pastor_2.photo, {
-        caption: `${CURRENT_LANGUAGE.pastorsInfo.title}\n${PERMISSIONS.info.pastor_2.name}\n${CURRENT_LANGUAGE.pastorsInfo.tel}${PERMISSIONS.info.pastor_2.tel}`,
+      await bot.sendPhoto(chatId, CONF.info.pastor_2.photo, {
+        caption: `${CURRENT_LANGUAGE.pastorsInfo.title}\n${CONF.info.pastor_2.name}\n${CURRENT_LANGUAGE.pastorsInfo.tel}${CONF.info.pastor_2.tel}`,
       });
       return bot.sendMessage(
         chatId,
-        `${CURRENT_LANGUAGE.pastorsInfo.moreInfo}\n${PERMISSIONS.urls.church}`,
+        `${CURRENT_LANGUAGE.pastorsInfo.moreInfo}\n${CONF.urls.church}`,
         { disable_web_page_preview: true }
       );
     }
     case CURRENT_LANGUAGE.mainMenu.liveStream: {
       return bot.sendMessage(
         chatId,
-        `${CURRENT_LANGUAGE.youtubeInfo}\n${PERMISSIONS.urls.youtube}`,
+        `${CURRENT_LANGUAGE.youtubeInfo}\n${CONF.urls.youtube}`,
         { disable_web_page_preview: true }
       );
     }
